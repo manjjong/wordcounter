b72f5a23b402d58b3ef4df1fe9d022d644b2038e
BuildTools <unconfigured@null.spigotmc.org>
2019-02-04 11:39:53 +0900

    first commit of wordcount proj

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\qkfka\onedrive\바탕 화면\웹프로그래밍\wordcount_1\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
